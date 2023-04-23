072c13c8f28beb279ab4e0cc8d3cf0bdd0aa3752
ramees <rameesrahath@gmail.com>
2023-04-24 00:22:28 +0530

    basic boilerplate with ts configuration

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import * as  _ from 'lodash';
+import './style.css';
+function component(){
+    const element = document.createElement("div");
+    element.innerHTML = _.join(['Hello','Webpack']);
+    element.classList.add('hello');
+    return element;
+}
+document.body.appendChild(component());
